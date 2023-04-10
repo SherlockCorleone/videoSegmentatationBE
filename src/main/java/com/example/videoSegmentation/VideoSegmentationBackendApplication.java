c17bfd19b09317e35a648dcd2fd5cbf92551ac25
sherlcok <MSIK@MSI>
2023-04-10 12:38:38 +0800

    init

---
+++ src/main/java/com/example/videoSegmentation/VideoSegmentationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.videoSegmentation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoSegmentationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoSegmentationBackendApplication.class, args);
+	}
+
+}
